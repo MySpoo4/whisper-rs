2f24e8e28ecb95580cade8893445aac93bb39834
Richard Wan <116916742+MySpoo4@users.noreply.github.com>
2024-08-10 03:44:33 -0400

    Update whisper_grammar.rs

--- src/whisper_grammar.rs
+++ src/whisper_grammar.rs
@@ -10,19 +10,19 @@ use whisper_rs_sys::{
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum WhisperGrammarElementType {
     /// End of rule definition
-    End = whisper_gretype_WHISPER_GRETYPE_END,
+    End = whisper_gretype_WHISPER_GRETYPE_END as i32,
     /// Start of alternate definition for a rule
-    Alternate = whisper_gretype_WHISPER_GRETYPE_ALT,
+    Alternate = whisper_gretype_WHISPER_GRETYPE_ALT as i32,
     /// Non-terminal element: reference to another rule
-    RuleReference = whisper_gretype_WHISPER_GRETYPE_RULE_REF,
+    RuleReference = whisper_gretype_WHISPER_GRETYPE_RULE_REF as i32,
     /// Terminal element: character (code point)
-    Character = whisper_gretype_WHISPER_GRETYPE_CHAR,
+    Character = whisper_gretype_WHISPER_GRETYPE_CHAR as i32,
     /// Inverse of a character(s)
-    NotCharacter = whisper_gretype_WHISPER_GRETYPE_CHAR_NOT,
+    NotCharacter = whisper_gretype_WHISPER_GRETYPE_CHAR_NOT as i32,
     /// Modifies a preceding [Self::Character] to be an inclusive range
-    CharacterRangeUpper = whisper_gretype_WHISPER_GRETYPE_CHAR_RNG_UPPER,
+    CharacterRangeUpper = whisper_gretype_WHISPER_GRETYPE_CHAR_RNG_UPPER as i32,
     /// Modifies a preceding [Self::Character] to add an alternate character to match
-    CharacterAlternate = whisper_gretype_WHISPER_GRETYPE_CHAR_ALT,
+    CharacterAlternate = whisper_gretype_WHISPER_GRETYPE_CHAR_ALT as i32,
 }
 
 impl From<whisper_rs_sys::whisper_gretype> for WhisperGrammarElementType {
